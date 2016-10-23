a76d1ed81014017f1371372babd0b2502c5e0d93
xFLy3r <datus3r@gmail.com>
2016-10-23 21:35:20 +0300

    Added validation and tested script

--- index.php
+++ index.php
@@ -8,15 +8,43 @@ use Gregwar\Captcha\CaptchaBuilder;
 use Respect\Validation\Validator as v;
 
 $builder = new CaptchaBuilder;
-$msg = '';
+$msgCaptcha = '';
+$msgName = '';
+$msgEmail = '';
+
+$usernameValidator = v::alnum()->noWhitespace()->length(3, 15);
+
+if (isset($_POST['name'])) {
+    if ($usernameValidator->validate($_POST['name'])) {
+        $validName = TRUE;
+    }
+    else {
+        $msgName = 'Wrong name or empty name';
+    }
+}
+
+if (isset($_POST['email'])) {
+    if(v::email()->validate($_POST['email'])) {
+        $validEmail = TRUE;
+    }
+    else {
+        $msgEmail = 'Wrong or empty email';
+    }
+
+}
 
 if (isset($_POST['phrase'])) {
     if($_POST['phrase'] == $_SESSION['phrase']) {
-
+        $validCaptcha = TRUE;
     }
     else {
-        $msg = 'Wrong or empty captcha';
+        $msgCaptcha = 'Wrong or empty captcha';
     }
+}
+
+if (isset($validName) && isset($validEmail) && isset($validCaptcha)) {
+    header('Location: helloworld.php');
+    exit();
 }
 
 $builder->build();
@@ -35,21 +63,23 @@ $_SESSION['phrase'] = $builder->getPhrase();
     <h3>GeekHub Homerwork #2</h3>
     <div class="row">
         <div class="four columns">
-            <label for="name">Your name</label>
+            <label class='label' for="name">Your name <span class="customText">(no whitespace and length between 3 and 15.)</span></label>
             <input class="u-full-width" placeholder="My Name" name="name" id="name" type="text" >
+            <?=$msgName?></p>
         </div>
     </div>
     <div class="row">
         <div class="four columns">
             <label for="email">Your email</label>
-            <input class="u-full-width" placeholder="myemail@example.com" name="email" id="email" type="email">
+            <input class="u-full-width" placeholder="myemail@example.com" name="email" id="email" type="text">
+            <?=$msgEmail?></p>
         </div>
     </div>
     <div class="row">
         <div class="four columns">
             <label for="phrase">Enter the Captcha</label>
             <p><img src="<?=$builder->inline(); ?>" /><br>
-            <?=$msg?></p>
+            <?=$msgCaptcha?></p>
             <input class="u-full-width" name="phrase" id="phrase" type="text" value="">
         </div>
     </div>
